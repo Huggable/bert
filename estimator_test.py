0bf54a57f4f631178c9761f37f7230fe9db86856
Huggable <771304257@qq.com>
2019-04-20 23:55:36 +0800

    2355

--- estimator_test.py
+++ estimator_test.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+import pandas as pd
 def model_fn_wrapper():
     def model_fn(features,labels,mode):
         x = tf.cast(features['x'],dtype=tf.float64)
@@ -24,35 +25,67 @@ def model_fn_wrapper():
     return model_fn
 
 
-estimator = tf.estimator.Estimator(model_fn=model_fn_wrapper(),model_dir='./d2')
+
 # define our data sets
 x_train = np.arange(0,40)
 y_train = x_train*2+1.01
 
 x_eval = np.arange(40,80)
 y_eval = x_eval*2+1.01
+def m(record):
 
-input_fn = tf.estimator.inputs.numpy_input_fn(
-    {"x": x_train,"y":y_train}, None, batch_size=4, num_epochs=None, shuffle=True)
-train_input_fn = tf.estimator.inputs.numpy_input_fn(
-    {"x": x_train,"y":y_train}, None, batch_size=4, num_epochs=1000, shuffle=False)
-eval_input_fn = tf.estimator.inputs.numpy_input_fn(
-    {"x": x_eval,"y":y_eval}, None, batch_size=4, num_epochs=1000, shuffle=False)
+    record['x'] = 2*record['x']
+    return record
 
-# train
-estimator.train(input_fn=input_fn, steps=1000)
-# Here we evaluate how well our model did.
-train_metrics = estimator.evaluate(input_fn=train_input_fn)
-eval_metrics = estimator.evaluate(input_fn=eval_input_fn)
-print("train metrics: %r" % train_metrics)
-print("eval metrics: %r" % eval_metrics)
-print(estimator.get_variable_names())
-print(estimator.get_variable_value('W'))
-print(estimator.get_variable_value('b'))
+def create_fn(x ,y=None,batch_size = 1,epoch = None):
+    def fn():
 
-estimator2 = tf.estimator.Estimator(model_fn=model_fn_wrapper(),model_dir='./d2')
-pre_input_fn = tf.estimator.inputs.numpy_input_fn(
-    {"x": x_train[0:1]},shuffle=False)
-r = estimator2.predict(input_fn=pre_input_fn)
-for s in r:
-    print(s)
+        dict = {}
+        dict['x'] = x
+        if y is not None:
+            dict['y']=y
+        dataset=tf.data.Dataset.from_tensor_slices(dict)
+        dataset = dataset.map(m)
+        dataset = dataset.batch(batch_size)
+
+        dataset  = dataset.repeat(epoch) if epoch is  not None else dataset.repeat()
+        iterator = dataset.make_one_shot_iterator()
+
+        featurs= iterator.get_next()
+        return featurs
+    return fn
+
+# input_fn = tf.estimator.inputs.numpy_input_fn(
+#     {"x": x_train,"y":y_train}, None, batch_size=4, num_epochs=None, shuffle=True)
+# train_input_fn = tf.estimator.inputs.numpy_input_fn(
+#     {"x": x_train,"y":y_train}, None, batch_size=4, num_epochs=1000, shuffle=False)
+# eval_input_fn = tf.estimator.inputs.numpy_input_fn(
+#     {"x": x_eval,"y":y_eval}, None, batch_size=4, num_epochs=1000, shuffle=False)
+
+
+def train():
+    # train
+    estimator = tf.estimator.Estimator(model_fn=model_fn_wrapper(), model_dir='./d2')
+    estimator.train(input_fn=create_fn(x_train,y_train,8), steps=3000)
+    # Here we evaluate how well our model did.
+    train_metrics = estimator.evaluate(input_fn=create_fn(x_train,y_train,16),steps = 1000)
+    eval_metrics = estimator.evaluate(input_fn=create_fn(x_eval,y_eval,16),steps = 1000)
+    print("train metrics: %r" % train_metrics)
+    print("eval metrics: %r" % eval_metrics)
+    print(estimator.get_variable_names())
+    print(estimator.get_variable_value('W'))
+    print(estimator.get_variable_value('b'))
+
+    estimator2 = tf.estimator.Estimator(model_fn=model_fn_wrapper(),model_dir='./d3')
+    estimator2.train(input_fn=create_fn(x_train, y_train, 8), steps=10)
+    train_metrics = estimator2.evaluate(input_fn=create_fn(x_train,y_train,16),steps = 1000)
+    eval_metrics = estimator2.evaluate(input_fn=create_fn(x_eval,y_eval,16),steps = 1000)
+    print("train metrics: %r" % train_metrics)
+    print("eval metrics: %r" % eval_metrics)
+    print(estimator2.get_variable_names())
+    print(estimator2.get_variable_value('W'))
+    print(estimator2.get_variable_value('b'))
+
+train()
+
+
